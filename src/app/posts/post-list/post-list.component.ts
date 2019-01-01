52aa13f3df7bffd23cdd2fbf5d9098d4864032c3
Ashley Fernandes <ashleefernandes@gmail.com>
2019-01-01 21:28:32 +0530

    added empty list validation"

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,13 +6,15 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./post-list.component.css"]
 })
 export class PostListComponent implements OnInit {
-  posts = [
-    { title: "This is title1", content: "This is content1" },
-    { title: "This is title2", content: "This is content2" },
-    { title: "This is title3", content: "This is content3" },
-    { title: "This is title4", content: "This is content4" }
-  ];
+  // posts = [
+  //   { title: "This is title1", content: "This is content1" },
+  //   { title: "This is title2", content: "This is content2" },
+  //   { title: "This is title3", content: "This is content3" },
+  //   { title: "This is title4", content: "This is content4" }
+  // ];
 
+  posts = [];
+  
   constructor() {}
 
   ngOnInit() {}
