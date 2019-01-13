1c2421e99c0ae24eeaafe404472bc370d6c7fed1
Ashley Fernandes <ashleefernandes@gmail.com>
2019-01-13 22:27:26 +0530

    added reactive form to post create component

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Output } from "@angular/core";
 import { Post } from "../post.model";
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: "app-post-create",
@@ -12,11 +13,14 @@ export class PostCreateComponent {
   ///
   @Output() postCreated = new EventEmitter<Post>();
 
-  onAddPost() {
-    const post: Post = {
-      title: this.enteredTitle,
-      content: this.enteredContent
-    };
-    this.postCreated.emit(post);
-  }
+  onAddPost(form:NgForm) {
+    if(form.invalid){
+      return ;
+    }
+        const post: Post = {
+          title: form.value.title,
+          content: form.value.content
+        };
+        this.postCreated.emit(post);
+    }
 }
